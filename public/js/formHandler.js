f3d04135b62f6754c0ff0c77f83d0455c88b34c6
Brent Scheibelhut <brent.scheibelhut@gmail.com>
2015-12-30 13:40:36 -0800

    Fixed online course processing

--- public/js/formHandler.js
+++ public/js/formHandler.js
@@ -1,63 +1,69 @@
+function createCourse(list, course, section, online) {
+    var li = $('<div/>')
+    .addClass('item')
+    .appendTo(list);
+
+    var i = $('<i/>')
+    .addClass('large github middle aligned icon')
+    .appendTo(li);
+
+    var content = $('<div/>')
+    .addClass('content')
+    .appendTo(li);
+
+    var header = $('<a/>')
+    .addClass('header')
+    .text(course.subject + " " + course.catalog_number + ' - ' + course.title)
+    .appendTo(content);
+
+    var time = description = '';
+    if (online) {
+        description = section.instructors[0] + " | Online";
+        time = 'Online';
+    } else {
+        description = section.instructors[0] + " | " + section.location.building + " " + section.location.room;
+
+        if (section.date.start_time) {
+            section.date.start_time = section.date.start_time.replace(/^0+/, '');
+        } else {
+            section.date.start_time = 'Unknown';
+        }
+
+        if (section.date.end_time) {
+            section.date.end_time = section.date.end_time.replace(/^0+/, '');
+        } else {
+            section.date.end_time = 'Unknown';
+        }
+
+        time = section.date.weekdays + " " +
+               section.date.start_time + ' - ' +
+               section.date.end_time;
+    }
+
+    var description = $('<div/>')
+    .addClass('description')
+    .text(description)
+    .appendTo(content);
+
+    var time = $('<div/>')
+    .addClass('description')
+    .text(time)
+    .appendTo(content);
+}
+
 function printResults(data) {
     var list = $("#courseList");
     list.empty();
     $.each(data, function (course) {
         course = data[course];
-        $.each(course.filteredClasses, function (section) {
-            section = course.filteredClasses[section];
-            console.log(section);
-
-            var li = $('<div/>')
-            .addClass('item')
-            .appendTo(list);
-
-            var i = $('<i/>')
-            .addClass('large github middle aligned icon')
-            .appendTo(li);
-
-            var content = $('<div/>')
-            .addClass('content')
-            .appendTo(li);
-
-            var header = $('<a/>')
-            .addClass('header')
-            .text(course.subject + " " + course.catalog_number + ' - ' + course.title)
-            .appendTo(content);
-
-            var time = description = '';
-            if (course.campus == 'ONLN ONLINE') {
-                description = section.instructors[0] + " | Online";
-                time = 'Online';
-            } else {
-                description = section.instructors[0] + " | " + section.location.building + " " + section.location.room;
-
-                if (section.date.start_time) {
-                    section.date.start_time = section.date.start_time.replace(/^0+/, '');
-                } else {
-                    section.date.start_time = 'Unknown';
-                }
-
-                if (section.date.end_time) {
-                    section.date.end_time = section.date.end_time.replace(/^0+/, '');
-                } else {
-                    section.date.end_time = 'Unknown';
-                }
-
-                time = section.date.weekdays + " " +
-                       section.date.start_time + ' - ' +
-                       section.date.end_time;
-            }
-
-            var description = $('<div/>')
-            .addClass('description')
-            .text(description)
-            .appendTo(content);
-
-            var time = $('<div/>')
-            .addClass('description')
-            .text(time)
-            .appendTo(content);
-        });
+        if (course.campus == 'ONLN ONLINE') {
+            createCourse(list, course, course.classes[0], true);
+        } else {
+            $.each(course.filteredClasses, function (section) {
+                section = course.filteredClasses[section];
+                createCourse(list, course, section);
+            });
+        }
     })
 }
 
