88782714b66096c013f33ffbfa2dc3f2876666bc
Brent Scheibelhut <brent.scheibelhut@gmail.com>
2015-12-30 13:32:20 -0800

    Got time working sort of

--- public/js/formHandler.js
+++ public/js/formHandler.js
@@ -1,3 +1,66 @@
+function printResults(data) {
+    var list = $("#courseList");
+    list.empty();
+    $.each(data, function (course) {
+        course = data[course];
+        $.each(course.filteredClasses, function (section) {
+            section = course.filteredClasses[section];
+            console.log(section);
+
+            var li = $('<div/>')
+            .addClass('item')
+            .appendTo(list);
+
+            var i = $('<i/>')
+            .addClass('large github middle aligned icon')
+            .appendTo(li);
+
+            var content = $('<div/>')
+            .addClass('content')
+            .appendTo(li);
+
+            var header = $('<a/>')
+            .addClass('header')
+            .text(course.subject + " " + course.catalog_number + ' - ' + course.title)
+            .appendTo(content);
+
+            var time = description = '';
+            if (course.campus == 'ONLN ONLINE') {
+                description = section.instructors[0] + " | Online";
+                time = 'Online';
+            } else {
+                description = section.instructors[0] + " | " + section.location.building + " " + section.location.room;
+
+                if (section.date.start_time) {
+                    section.date.start_time = section.date.start_time.replace(/^0+/, '');
+                } else {
+                    section.date.start_time = 'Unknown';
+                }
+
+                if (section.date.end_time) {
+                    section.date.end_time = section.date.end_time.replace(/^0+/, '');
+                } else {
+                    section.date.end_time = 'Unknown';
+                }
+
+                time = section.date.weekdays + " " +
+                       section.date.start_time + ' - ' +
+                       section.date.end_time;
+            }
+
+            var description = $('<div/>')
+            .addClass('description')
+            .text(description)
+            .appendTo(content);
+
+            var time = $('<div/>')
+            .addClass('description')
+            .text(time)
+            .appendTo(content);
+        });
+    })
+}
+
 function submitHandler(form, action, msgClass, msg) {
     var isFormValid = true;
 
@@ -16,8 +79,7 @@ function submitHandler(form, action, msgClass, msg) {
         $.post(action, $(form).serialize())
         .done(function (data) {
             // $("#loading").fadeOut(500);
-            console.log(data);
-            $("#response").text(data);
+            printResults(data);
             // if (data == "Success") {
             //     successMsg(msg, msgClass);
             // } else {
--- routes/questSearch.js
+++ routes/questSearch.js
@@ -1,28 +1,59 @@
 require('dotenv').load();
 var uwapi = require('uwapi')(process.env.UW_API_TOKEN);
 var _ = require('lodash');
-var data1151 = require('../cronjobs/1151.json');
+var data = require('../cronjobs/1151.json');
 
 exports = module.exports = function(req, res) {
 	var options = req.body;
+	console.log(options);
+	var returnData = {};
+	var onlineCourse = false;
+	var start_time = '';
+	var end_time = '';
+
+	if (options.campus == 'ONLN ONLINE') {
+		onlineCourse = true;
+	}
+
+	if (options.time) {
+		time = options.time.split('-');
+		start_time = time[0];
+		end_time = time[1];
+	}
 	//Currently have options.term, options.campus, options.days
 	if (options.campus) {
-		data = _filter(data, function (course) {
+		returnData = _.filter(data, function (course) {
 			return course.campus == options.campus;
 		});
 	}
 
+	// console.log(returnData);
 	//Filter by day
-	if (options.days) {
-		data = _filter(data, function (course) {
-			for (var i=0; i < course.classes.length; i++) {
-				classInfo = course.classes[i];
-				if (classInfo.dates.weekdays == options.days) {
-					return true
+	if (options.days && !onlineCourse) {
+		//We should be able to do this in one pass - just not sure how with lodash yet
+
+		//For each course, search if any of it's classes match
+		_.each(returnData, function (course, index) {
+			returnData[index].filteredClasses = _.filter(returnData[index].classes, function (section) {
+				if (section.date.weekdays == options.days) {
+					if (options.time) {
+						if (section.date.start_time == start_time && section.date.end_time == end_time) {
+							return true;
+						}
+					} else {
+						return true;
+					}
 				}
-			}
+				return false;
+			});
+		});
+
+		//Remove all courses that have no classes
+		returnData = _.filter(returnData, function (course) {
+			return course.filteredClasses.length != 0;
 		});
 	}
 
-	res.json(data);
+	// console.log(returnData);
+	res.json(returnData);
 };
