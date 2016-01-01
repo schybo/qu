cbc84968456cd79b59f581aa4ec1fa31d10fafdf
Brent Scheibelhut <brent.scheibelhut@gmail.com>
2015-12-31 17:05:34 -0800

    Switched over to knockout for rendering list

--- public/js/home.js
+++ public/js/home.js
@@ -1,34 +1,101 @@
 $(document).ready(function () {
-    $("#questForm").submit(function(e) {
-        submitHandler("#questForm", $(this).attr('action'));
-        return false;
-    });
     $('.ui.search.dropdown').dropdown();
 });
 
 var ViewModel = function() {
     //We can do this after the document.ready to get the term if necessary
-    this.campus = ko.observable('');
-    this.days = ko.observable('');
-    this.dayTime = ko.observable('');
+    var self = this;
+    self.campus = ko.observable('');
+    self.days = ko.observable('');
+    self.dayTime = ko.observable('');
+    self.courses = ko.observableArray();
 
-    this.onlineAbroad = ko.pureComputed(function() {
-        if (this.campus() == 'ONLN ONLINE' || this.campus() == 'OFF ABROAD') {
-            this.days('');
-            this.dayTime('');
+    self.onlineAbroad = ko.pureComputed(function() {
+        if (self.campus() == 'ONLN ONLINE' || self.campus() == 'OFF ABROAD') {
+            self.days('');
+            self.dayTime('');
             return true;
         } else {
             return false;
         }
     }, this);
 
-    this.shortCourse = ko.pureComputed(function() {
-        return this.days() == "MWF";
+    self.shortCourse = ko.pureComputed(function() {
+        return self.days() == "MWF";
     }, this);
 
-    this.superLongCourse = ko.pureComputed(function() {
-        return this.days() == "W" || this.days() == "T";
+    self.superLongCourse = ko.pureComputed(function() {
+        return self.days() == "W" || self.days() == "T";
     }, this);
+
+    var createCourse = function(course, section, online, abroad) {
+        newCourse = {};
+        newCourse.titleText = course.subject + " " + course.catalog_number + ' - ' + course.title;
+
+        if (online) {
+            newCourse.profLocationText = section.instructors[0] + " | Online";
+            newCourse.timeText = 'Online';
+        } else if (abroad) {
+            newCourse.profLocationText = "Not Applicable | Abroad";
+            newCourse.timeText = 'Abroad';
+        } else {
+            newCourse.profLocationText = section.instructors[0] + " | " + section.location.building + " " + section.location.room;
+
+            if (section.date.start_time) {
+                section.date.start_time = section.date.start_time.replace(/^0+/, '');
+            } else {
+                section.date.start_time = 'Unknown';
+            }
+
+            if (section.date.end_time) {
+                section.date.end_time = section.date.end_time.replace(/^0+/, '');
+            } else {
+                section.date.end_time = 'Unknown';
+            }
+
+            newCourse.timeText = section.date.weekdays + " " +
+                   section.date.start_time + ' - ' +
+                   section.date.end_time;
+        }
+
+        return newCourse;
+    }
+
+    var createCourseList = function (data) {
+        var tempCourseList = [];
+        $.each(data, function (course) {
+            course = data[course];
+            if (course.campus == 'ONLN ONLINE') {
+                tempCourseList.push(createCourse(course, course.classes[0], true));
+            } else if (course.campus == 'OFF ABROAD') {
+                tempCourseList.push(createCourse(course, course.classes[0], false, true));
+            } else {
+                $.each(course.filteredClasses, function (section) {
+                    section = course.filteredClasses[section];
+                    tempCourseList.push(createCourse(course, section));
+                });
+            }
+        })
+        self.courses(tempCourseList);
+    }
+
+    self.submit = function () {
+        console.log("Hello");
+        var form = $("#questForm");
+        var action = form.attr('action');
+
+        $("form").addClass('loading');
+        $.post(action, $(form).serialize())
+        .done(function (data) {
+            $("form").removeClass('loading');
+            createCourseList(data);
+        })
+        .fail(function (data) {
+            $("form").removeClass('loading');
+            //Should issue an error here
+            createCourseList(data);
+        })
+    }
 };
 
 ko.applyBindings(new ViewModel()); 
