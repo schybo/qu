4492e0c780861901156fb797e3dd30dc073b66d4
Brent Scheibelhut <brent.scheibelhut@gmail.com>
2016-11-17 10:28:36 -0500

    Fixed cron timing?

--- cronjobs/compilationCron.py
+++ cronjobs/compilationCron.py
@@ -39,7 +39,7 @@ terms = allTermInfo['listings']
 ###### NOTE: ALL PATHS ARE FROM THE TOP LEVEL DIRECTORY AS THESE RUN ON HEROKU ########
 
 # Cronjob for current term courses
-@sched.scheduled_job('interval', hours=6)
+@sched.scheduled_job('interval', hour=6)
 def generateCoursesForCurrentTerm():
 	print "Generating courses for current and next term"
 	terms = [str(allTermInfo['current_term']), str(allTermInfo['next_term'])]
